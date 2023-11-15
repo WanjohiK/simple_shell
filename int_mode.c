de90f48ff0fc591d767e36d12261c9f2156c1e7b
WanjohiK <kelvinnyawira2022@gmail.com>
2023-11-15 14:25:21 +0300

    shell

---
+++ int_mode.c
@@ -0,0 +1,75 @@
+#include "shell.h"
+
+/**
+ * interactive - should show if shell is interacting
+ * @info: this should check struct address
+ *
+ * Return: 1 succes and otherwise 0
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - checks delimeter
+ * @c: this will checked
+ * @delim: this checks the delimeter
+ * Return: 1 success otherwise 0
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+
+/**
+ * _isalpha - to check
+ * @c: we check this
+ * Return: 1 success and 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * _atoi - this will convert the string to an integer
+ * @s: this is the string that we want converted
+ * Return: 0 if sucessful
+ */
+
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
+}
+
